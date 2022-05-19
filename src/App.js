6aaad64f9d1bebbcd7d03584f7a805be43c81515
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-05-19 16:27:24 -0400

    traffic_light

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [on, setOn] = useState ({
+    red: false,
+    yellow: false,
+    green: false
+  });
+
+  const handleClick = (e) => {
+    if (e.target.id === "first") {
+      setOn ({
+        red: true,
+        yellow: false,
+        green: false
+      })}
+    else if (e.target.id === "second") {
+      setOn ({
+        red: false,
+        yellow: true,
+        green: false
+      })}
+      else (
+        setOn ({
+          red: false,
+          yellow: false,
+          green: true
+        }))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container d-flex justify-content-center">
+        <div className="traffic-light d-flex flex-column">
+          <span>
+            <button className= {`circle1 ${on.red ? "light-on":""}`}  id= "first" onClick={handleClick}></button>
+          </span>
+          <br></br>
+          <span>
+            <button className={`circle2 ${on.yellow ? "light-on":""}`} id= "second" onClick={handleClick}></button>
+          </span>
+          <br></br>
+          <span>
+            <button className={`circle3 ${on.green ? "light-on":""}`} id= "third" onClick={handleClick}></button>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
